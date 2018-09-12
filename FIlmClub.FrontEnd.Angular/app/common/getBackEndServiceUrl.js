00fc4e5e19dd09b1f2d6216362cf45b82cbe0f4b
Shane <superzinbo@gmail.com>
2018-09-12 23:51:20 +0100

    Fix url

--- FIlmClub.FrontEnd.Angular/app/common/getBackEndServiceUrl.js
+++ FIlmClub.FrontEnd.Angular/app/common/getBackEndServiceUrl.js
@@ -1,7 +1,7 @@
 export default class GetBackEndServiceUrl {
     constructor($location) {
         const host = $location.host();
-        this.backEndServiceUrl = "https://filmclub-java-dropwizard.herokuapp.com/";
+        this.backEndServiceUrl = "https://filmclub-java-dropwizard.herokuapp.com";
         if(host.includes("localhost")) this.backEndServiceUrl = "http://localhost:9000";
     }
 
